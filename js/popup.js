bad482e47da533c67c9c52558154cf6ae2e25ce2
Denys <80397210+juntapowerrr@users.noreply.github.com>
2023-04-07 00:26:10 +0300

    scroll-lock

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,10 @@
 $('.open-popup').click(function(e) {
     e.preventDefault();
-    $('.popup-bg').fadeIn(500);
-    $('html').addClass('no-scroll');
+    $('.popup-bg').fadeIn(300);
+    $('body').addClass('no-scroll');
 });
 
 $('.close-popup').click(function() {
-    $('.popup-bg').fadeOut(800);
-    $('html').removeClass('no-scroll');
+    $('.popup-bg').fadeOut(300);
+    $('body').removeClass('no-scroll');
 });

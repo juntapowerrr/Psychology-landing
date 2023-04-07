033deb3df9dcf42849b82345e9fe41d7cd686af4
Denys <80397210+juntapowerrr@users.noreply.github.com>
2023-04-07 18:15:43 +0300

    popup-bg-blur

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,12 @@
 $('.open-popup').click(function(e) {
     e.preventDefault();
     $('.popup-bg').fadeIn(300);
+    //$('.popup-bg').addClass('do-scroll');
     $('body').addClass('no-scroll');
 });
 
 $('.close-popup').click(function() {
     $('.popup-bg').fadeOut(300);
+    //$('.popup-bg').removeClass('do-scroll');
     $('body').removeClass('no-scroll');
 });
